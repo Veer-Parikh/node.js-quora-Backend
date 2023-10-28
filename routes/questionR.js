d3842ac539dad21242b8fe423f4a0352ab311a35
Veer-Parikh <parikhveer71@gmail.com>
2023-10-29 01:55:13 +0530

    filtering added

--- routes/questionR.js
+++ routes/questionR.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { askQuestion,printQuestions } = require('../controllers/questionC')
+const { askQuestion,printQuestions,filterByCategory,filterByUserId } = require('../controllers/questionC')
 const authenticatetoken = require('../authenticate/auth')
 
-router.post('/askQuestion',authenticatetoken,askQuestion)
+router.post('/askQuestion',askQuestion)
 router.get('/getQuestions',authenticatetoken,printQuestions)
+router.post('/filter1',filterByCategory)
+router.post('/filter2',filterByUserId)
 
 module.exports = router;
