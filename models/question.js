4cf9e6d1ee2de1ea2dd67a28f93b0cb098c495bd
Veer-Parikh <parikhveer71@gmail.com>
2023-10-29 01:55:35 +0530

    filtering added

--- models/question.js
+++ models/question.js
@@ -15,14 +15,14 @@ const questionSchema=new schema({
         ref : 'User',
         required : true
     },
-    upvote:{
-        type: schema.Types.ObjectId,
-        ref:'User',default:0
-    },
-    downvote:{
-        type: schema.Types.ObjectId,
-        ref:'User',default:0
-    }
+    // upvote:{
+    //     type: schema.Types.ObjectId,
+    //     ref:'User',default:0
+    // },
+    // downvote:{
+    //     type: schema.Types.ObjectId,
+    //     ref:'User',default:0
+    // }
 })
 
 module.exports=mongoose.model('Question',questionSchema)
